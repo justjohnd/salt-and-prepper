b138890e30a63bba73075df5022b9a7871828689
John Denlinger <justjohnd@gmail.com>
2021-12-01 16:35:21 +0900

    Move all data scrubbing prior to setting meal data during fetch

--- src/components/MealList.jsx
+++ src/components/MealList.jsx
@@ -1,121 +1,21 @@
 import React, { useState, useEffect } from 'react';
-
 import Meal from './Meal';
-import KEYWORDS, {
-  DONT_INCLUDE,
-  USED_INGREDIENT_COUNT,
-  DONT_ADD_CALORIES,
-  MUST_ADD_CALORIES,
-  BAD_API_IDS,
-} from '../findWord';
 
 function MealList(props) {
-  const keywordsSeen = [];
-  const results = props.meals.results;
-  const rejectionReason = [];
-  let caloriesArray = [];
-  let caloriesTotal = "";
-
-
-    // Scrub data
-    const resultsCopy = [...results];
-    const scrubbedResults = resultsCopy.map((result) => {
-      result.dontInclude = false;
-      result.addCalories = false;
-
-      if (BAD_API_IDS[result.id]) {
-        result.dontInclude = true;
-        rejectionReason.push(
-          `Rejection Reason: Based on recipe ${result.id}, this recipe is missing kinformation.`
-        );
-      }
-
-     if (dontIncludeCheck(result.title.toLowerCase())) {
-       result.dontInclude = true;
-     };
-
-      if (addCaloriesCheck(result.title.toLowerCase())) {
-        result.addCalories = true;
-        let calories = result.nutrition.nutrients[0].amount;
-        calories += 200;
-      };
-
-      return result;
-    });
-
-    const filteredResults = scrubbedResults.filter(result => {
-      if (result.dontInclude === false) {
-        return result;
-      }
-    });
-
-    if (filteredResults[0] === undefined) {
-      console.log(filteredResults);
-    } else {
-      caloriesArray = filteredResults.map(result => result.nutrition.nutrients[0].amount);
-      caloriesTotal = caloriesArray.reduce((prev, cur) => prev + cur).toFixed(0);
-    }
-
-  function dontIncludeCheck(string) {
-    const words = string.split(' ');
-
-    for (const word of words) {
-      if (KEYWORDS[word]) {
-        if (keywordsSeen === []) {
-          keywordsSeen.push(word);
-        } else {
-          keywordsSeen.forEach((e) => {
-            if (e === word) {
-              console.log(
-                `Rejection Reason: '${string} is similar or identical recipe already being used`
-              );
-              return true;
-            } else {
-              keywordsSeen.push(word);
-            }
-          });
-        }
-      }
-
-      if (DONT_INCLUDE[word]) {
-        rejectionReason.push(
-          `Title (${string})indicates that this food is a desert, therfore don't include`
-        );
-        return true;
-      }
-    }
-  }
-
-  function addCaloriesCheck(string) {
-    const words = string.split(' ');
-        for (const word of words) {
-          if (MUST_ADD_CALORIES[word]) {
-            console.log("calories added to meal");
-            return true;
-          } else {
-          console.log("no calories added to meal");
-          return false;
-        }
-  }
-}
 
   return (
     <main>
       <section className="nutrients">
         {/* Calculates TOTALS for all meals generate */}
-        <h1>Total Calories: {caloriesTotal}</h1>
-        {filteredResults[0] === undefined && <h1>Sorry, no results matched criteria</h1>}
+        {/* <h1>Total Calories: {caloriesTotal}</h1> */}
+        {props.meals[0] === undefined && <h1>Sorry, no results matched criteria</h1>}
 
       </section>
 
       <section className="meals">
-        {filteredResults.map(meal => {
+        {props.meals.map(meal => {
           const [calories, protein, fat, carbohydrates, sugar] =
             meal.nutrition.nutrients;
-
-            console.log(meal);
-            console.log("filtered:");
-            console.log(filteredResults);
 
           return (
             <Meal
--- src/components/MealPlan.jsx
+++ src/components/MealPlan.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import MealList from './MealList';
 import Checkbox from './Checkbox';
+import KEYWORDS, {
+  DONT_INCLUDE,
+  MUST_ADD_CALORIES,
+  BAD_API_IDS,
+} from '../findWord';
 
 
 function MealPlan(props) {
-  const ids = [];
-  const totals = [];
   let runningCalTally = 0;
-  let target = 1000; // Set this for testing. Typiocally props.userCalAverage;
+  let target = 700; // Set this for testing. Typiocally props.userCalAverage;
 
   const DIETS = [
     'Vegetarian',
@@ -41,12 +44,133 @@ function MealPlan(props) {
         
     function getMeals() {   
       fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=627d3d5f6ac5413fb693db5fb5a4d394&diet=vegetarian&type=main course,side dish,snack,appetizer,salad,soup,fingerfood&excludeIngredients=white chocolate,vanilla bean paste,semi sweet chocolate chips&fillIngredients=true&instructionsRequired=true&maxReadyTime=30&maxSugar=10&minProtein=1&minCarbs=1&minFat=1&minCalories=1&maxCalories=${props.userCalAverage}&sort=random&number=1`
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=627d3d5f6ac5413fb693db5fb5a4d394&diet=vegetarian&type=main course,side dish,snack,appetizer,salad,soup,fingerfood&excludeIngredients=white chocolate,vanilla bean paste,semi sweet chocolate chips&fillIngredients=true&instructionsRequired=true&maxReadyTime=30&maxSugar=10&minProtein=1&minCarbs=1&minFat=1&minCalories=1&maxCalories=${props.userCalAverage}&sort=random&number=3`
       )
         .then((response) => response.json())
         .then(data => {
+          const keywordsSeen = [];
+          const rejectionReason = [];
 
-          setMeals(data);
+          const scrubbedResults = data.results.map(result => {
+            result.dontInclude = false;
+            result.addCalories = false;
+
+            if (BAD_API_IDS[result.id]) {
+              result.dontInclude = true;
+              rejectionReason.push(
+                `Rejection Reason: Based on recipe ${result.id}, this recipe is missing kinformation.`
+              );
+            }
+
+            if (dontIncludeCheck(result.title.toLowerCase())) {
+              result.dontInclude = true;
+            }
+
+            if (addCaloriesCheck(result.title.toLowerCase())) {
+              result.addCalories = true;
+              let calories = result.nutrition.nutrients[0].amount;
+              calories = calories + 200;
+            }
+
+            return result;
+          });
+
+          const filteredResults = scrubbedResults.filter(result => {
+            if (result.dontInclude === false) {
+              return result;
+            }
+          });
+
+          function dontIncludeCheck(string) {
+            const words = string.split(' ');
+
+            for (const word of words) {
+              if (KEYWORDS[word]) {
+                if (keywordsSeen === []) {
+                  keywordsSeen.push(word);
+                } else {
+                  keywordsSeen.forEach(e => {
+                    if (e === word) {
+                      console.log(
+                        `Rejection Reason: '${string} is similar or identical recipe already being used`
+                      );
+                      return true;
+                    } else {
+                      keywordsSeen.push(word);
+                    }
+                  });
+                }
+              }
+
+              if (DONT_INCLUDE[word]) {
+                rejectionReason.push(
+                  `Title (${string})indicates that this food is a desert, therfore don't include`
+                );
+                return true;
+              }
+            }
+          }
+
+          function addCaloriesCheck(string) {
+            const words = string.split(' ');
+            for (const word of words) {
+              if (MUST_ADD_CALORIES[word]) {
+                console.log('calories added to meal');
+                return true;
+              } else {
+                console.log('no calories added to meal');
+                return false;
+              }
+            }
+          }
+
+          return filteredResults;
+        })
+        .then((filteredResults) => {
+          // Create an array of calories for each meal to pass to combinations function
+          const caloriesArray = filteredResults.map(result => {
+            return result.nutrition.nutrients[0].amount;
+          });
+
+          // Find all combinations of meals
+          const findCombinations = array => {
+            const output = [];
+            if (array.length === 1) {
+              output.push([array[0]]);
+            } else {
+            for (let i = 0; i <= array.length - 1; i++) {
+              let firstPosition = array[i];
+              for (let j = i + 1; j <= array.length - 1; j++) {
+                let secondPosition = array[j];
+                output.push([firstPosition, i, secondPosition, j]);
+              }
+            }
+          }
+            return output;
+          };
+
+          const calorieComboArrays = findCombinations(caloriesArray);
+          console.log(calorieComboArrays);
+
+          const diffFromTarget = calorieComboArrays.map(array => {
+            const calorieArrayTotal = array.reduce((prev, cur) => prev + cur).toFixed(0);
+            array.push(Math.abs(calorieArrayTotal - target));
+            return array;
+          });
+
+          const differences = diffFromTarget.map((array) => array.at(-1));
+          const closestToTarget = differences.indexOf(Math.min(...differences));
+          const bestCombo = diffFromTarget[closestToTarget];
+
+          const filtered = filteredResults.filter((result, index) => {
+            if (index === bestCombo[1] || index === bestCombo[3]) {
+              return result;
+            }
+          });
+
+          console.log(filtered);
+          
+          setMeals(filtered);
         })
         .catch(() => {
           console.log(`Error`);
