b9094afadb49250c26d8e037805a3fafd7469bfb
John Denlinger <justjohnd@gmail.com>
2021-12-09 11:09:03 +0900

    Style recipe containers

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -8,50 +8,67 @@ export default function Meal(props) {
     <div className="recipe">
       {props.meal && (
         <div>
-          <h2 className="title">{props.meal.title}</h2>
-          <img src={props.meal.image} alt="recipe" />
-          <ul className="instructions">
-            <li>
-              <strong>Calories: </strong>
-              {props.calories}
-            </li>
-            <li>
-              <strong>Protein: </strong>
-              {props.protein}
-            </li>
-            <li>
-              <strong>Carbohydrates: </strong>
-              {props.carbohydrates}
-            </li>
-            <li>
-              <strong>Fat: </strong>
-              {props.fat}
-            </li>
-            <li>
-              <strong>Sugar: </strong>
-              {props.sugar}
-            </li>
-            <li>
-              <strong>
-                {props.addCalories && 'Calories were added to this meal'}
-              </strong>
-            </li>
-          </ul>
+          <div className="container-title">
+            <h2 className="title">{props.meal.title}</h2>
+          </div>
+          <div className="img-and-nutrition">
+            <img src={props.meal.image} alt="recipe" />
+            <ul className="instructions">
+              <li>
+                <strong>Calories: </strong>
+                {props.calories}
+              </li>
+              <li>
+                <strong>Protein: </strong>
+                {props.protein}
+              </li>
+              <li>
+                <strong>Carbohydrates: </strong>
+                {props.carbohydrates}
+              </li>
+              <li>
+                <strong>Fat: </strong>
+                {props.fat}
+              </li>
+              <li>
+                <strong>Sugar: </strong>
+                {props.sugar}
+              </li>
+              <li>
+                <strong>
+                  {props.addCalories && 'Calories were added to this meal'}
+                </strong>
+              </li>
+            </ul>
+          </div>
 
-          <button onClick={() => props.handleIngredientsCallback(props.index)}>
-            {!props.ingredientsDisplay[props.index] ? 'Show Ingredients' : 'Hide Ingredients'}
+          <button
+            className="btn-primary"
+            onClick={() => props.handleIngredientsCallback(props.index)}
+          >
+            {!props.ingredientsDisplay[props.index]
+              ? 'Show Ingredients'
+              : 'Hide Ingredients'}
           </button>
           <section className="ingredients-section">
-            {props.ingredientsDisplay[props.index] && <Ingredients recipes={props.recipes} meal={props.meal} />}
+            {props.ingredientsDisplay[props.index] && (
+              <Ingredients recipes={props.recipes} meal={props.meal} />
+            )}
           </section>
 
-          <button onClick={() => props.handleInstructionsCallback(props.index)}>
-            {!props.instructionsDisplay[props.index] ? 'Show Instructions' : 'Hide Instructions'}
+          <button
+            className="btn-primary"
+            onClick={() => props.handleInstructionsCallback(props.index)}
+          >
+            {!props.instructionsDisplay[props.index]
+              ? 'Show Instructions'
+              : 'Hide Instructions'}
           </button>
           <section className="ingredients-section">
-            {props.instructionsDisplay[props.index] && <Instructions recipes={props.recipes} meal={props.meal} />}
+            {props.instructionsDisplay[props.index] && (
+              <Instructions recipes={props.recipes} meal={props.meal} />
+            )}
           </section>
-
         </div>
       )}
     </div>
